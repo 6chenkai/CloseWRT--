374f8cbb0b94fbea6020bf56de0d4d2d1756a889
zongheng23333 <1954640276@qq.com>
2024-03-24 21:42:47 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -8,10 +8,7 @@ rm -rf    feeds/luci/applications/luci-app-store
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-echo >> feeds.conf.default
-echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-./scripts/feeds update
 
-rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb,luci-app-store}
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
 ./scripts/feeds install -a  
 
