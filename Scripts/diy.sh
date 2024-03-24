50f6c98c80b595b5013b8e97b0037a207f70bd67
zongheng23333 <1954640276@qq.com>
2024-03-24 15:57:58 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -4,7 +4,6 @@
 
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-git pull
 ./scripts/feeds update -a
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
