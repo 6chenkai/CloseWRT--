71d1feb3c774e9936bd522c626c4c0996ac31c43
zongheng23333 <1954640276@qq.com>
2024-03-24 16:00:25 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+rm -rf    feeds/luci/applications/luci-app-openclash
+rm -rf    feeds/luci/applications/luci-app-passwall
+rm -rf    feeds/luci/applications/luci-app-ssr-plus
 
 
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-./scripts/feeds update -a
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+
+./scripts/feeds update
+
+rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb}
+./scripts/feeds install -a  
 ./scripts/feeds install -a
