943fa385465b9c814556f87575f42c58f3f7ca16
zongheng23333 <1954640276@qq.com>
2024-05-20 12:24:51 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -5,12 +5,14 @@ rm -rf    feeds/luci/applications/luci-app-passwall
 rm -rf    feeds/luci/applications/luci-app-ssr-plus
 rm -rf    feeds/luci/applications/luci-app-store
 
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 ./scripts/feeds update -a
 rm -rf feeds/smpackage/{base-files,dnsmasq*,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb,luci-app-store}
-
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 rm -rf    feeds/luci/applications/luci-app-openclash
 rm -rf    feeds/luci/applications/luci-app-passwall
 rm -rf    feeds/luci/applications/luci-app-ssr-plus
