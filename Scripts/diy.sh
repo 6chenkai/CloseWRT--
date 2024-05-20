e001e9666be16a6f091658a488c176235e863c4f
zongheng23333 <1954640276@qq.com>
2024-05-20 13:43:32 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -4,24 +4,23 @@ rm -rf    feeds/luci/applications/luci-app-openclash
 rm -rf    feeds/luci/applications/luci-app-passwall
 rm -rf    feeds/luci/applications/luci-app-ssr-plus
 rm -rf    feeds/luci/applications/luci-app-store
-
-
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-./scripts/feeds update -a
-rm -rf feeds/smpackage/{base-files,dnsmasq*,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd-alt,miniupnpd-iptables,wireless-regdb,luci-app-store}
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
 rm -rf    feeds/luci/applications/luci-app-openclash
 rm -rf    feeds/luci/applications/luci-app-passwall
 rm -rf    feeds/luci/applications/luci-app-ssr-plus
 
 
-cp -rf  feeds/smpackage/luci-app-openclash    feeds/luci/applications/luci-app-openclash
-cp -rf  feeds/smpackage/luci-app-passwall    feeds/luci/applications/luci-app-passwall
-cp -rf  feeds/smpackage/luci-app-ssr-plus    feeds/luci/applications/luci-app-ssr-plus
-cp -rf  feeds/smpackage/luci-app-mosdns      feeds/luci/applications/luci-app-mosdns
+cp -rf  feeds/kenzo/luci-app-openclash    feeds/luci/applications/luci-app-openclash
+cp -rf  feeds/kenzo/luci-app-passwall    feeds/luci/applications/luci-app-passwall
+cp -rf  feeds/kenzo/luci-app-ssr-plus    feeds/luci/applications/luci-app-ssr-plus
+cp -rf  feeds/kenzo/luci-app-mosdns      feeds/luci/applications/luci-app-mosdns
 
 echo >> feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
