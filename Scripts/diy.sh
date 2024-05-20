f6d672469d98f30279aa93926fcf10e806e77520
zongheng23333 <1954640276@qq.com>
2024-05-20 20:33:48 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-
+ sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 ./scripts/feeds update -a
-
+ rm -rf feeds/luci/applications/luci-app-mosdns
+ rm -rf feeds/luci/applications/luci-app-passwall
+ rm -rf feeds/luci/applications/luci-app-ssr-plus
+ rm -rf feeds/luci/applications/luci-app-openclash
+ 
+ cp -rf feeds/smpackage/luci-app-openclash   feeds/luci/applications/luci-app-openclash
+ cp -rf feeds/smpackage/luci-app-ssr-plus   feeds/luci/applications/luci-app-ssr-plus
+ cp -rf feeds/smpackage/luci-app-passwall   feeds/luci/applications/luci-app-passwall
+ cp -rf feeds/smpackage/luci-app-mosdns  feeds/luci/applications/luci-app-mosdns
+ 
+ rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+ rm -rf feeds/packages/utils/v2dat
+ 
 ./scripts/feeds install -a  
 
