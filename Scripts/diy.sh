417ca04d9b340c52dc65b6b8e301641c4df98527
zongheng23333 <1954640276@qq.com>
2024-05-21 13:37:42 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
  sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 ./scripts/feeds update -a 
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
 
-
-rm -rf  feeds/luci/applications/luci-app-openclash
-cp -rf  feeds/smpackage/luci-app-openclash   feeds/luci/applications/luci-app-openclash
-
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 ./scripts/feeds install -a  
 
