6863348cadffb5774bfba399eddc76adfd37091c
zongheng23333 <1954640276@qq.com>
2024-05-21 14:23:52 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
  sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 ./scripts/feeds update -a 
+rm -rf feeds/packages/luci-app-clash
+cp -rf feeds/smpackage/luci-app-clash   feeds/packages/luci-app-clash
+
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
