2715a02e71cff74b4d838861a4ca4816d5b885b9
zongheng23333 <1954640276@qq.com>
2024-05-21 19:26:08 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -4,12 +4,5 @@
 rm -rf feeds/luci/applications/luci-app-clash
 cp -rf feeds/smpackage/luci-app-clash   feeds/luci/applications/luci-app-clash
 
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
-
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 ./scripts/feeds install -a  
 
