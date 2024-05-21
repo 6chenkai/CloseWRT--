4a7eae05e333f37d77fe3f3ed4891c1eb04af916
zongheng23333 <1954640276@qq.com>
2024-05-21 21:41:07 +0800

    更新 diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
  sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 ./scripts/feeds update -a 
-rm -rf feeds/luci/applications/luci-app-clash
-cp -rf feeds/smpackage/luci-app-clash   feeds/luci/applications/luci-app-clash
+rm -rf feeds/luci/applications/luci-app-openclash
+cp -rf feeds/smpackage/luci-app-openclash   feeds/luci/applications/luci-app-openclash
 
 ./scripts/feeds install -a  
 
