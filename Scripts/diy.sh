7551eea6040b82f9574bb7506036d92e00341e88
zongheng23333 <1954640276@qq.com>
2024-05-21 12:52:58 +0800

    Update diy.sh

--- Scripts/diy.sh
+++ Scripts/diy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-./scripts/feeds update -a   && rm -rf feeds/luci/applications/luci-app-mosdns
+./scripts/feeds update -a 
 
 
 rm -rf  feeds/luci/applications/luci-app-openclash
